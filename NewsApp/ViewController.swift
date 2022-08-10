8f8bf861400172e6a22064243e69977d9a6ee8f1
Emmanuel Mensah <immaxp183@gmail.com>
2022-08-10 03:32:57 +0200

    Building my user interface table view and the detail view

--- NewsApp/ViewController.swift
+++ NewsApp/ViewController.swift
@@ -8,20 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
     var model = ArticleModel()
     var articles = [Article]()
    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Table view delegate and datasource
+        tableView.delegate = self
+        tableView.dataSource = self
         // Get the articles from the model
         model.delegate = self
         model.getArticles()
     }
   }
 
+extension ViewController:UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return articles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //Get a cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleCell", for: indexPath)
+        //Customize cell
+        
+        //Return the cell
+        return cell
+    }
+    
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        <#code#>
+//    }
+}
 extension ViewController:ArticleModelProtocol{
     // Article Model Protocol Methods
     func articleRetrieved(_ article: [Article]) {
         self.articles = article
+        //Refresh data
+        tableView.reloadData()
     }
 }
+
